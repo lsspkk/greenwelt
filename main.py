253b14b3130a2e7aec57af3ddfd9083ea9a9b47e
lsspkk <lsspkk@gmail.com>
2026-01-27 22:03:02 +0200

    stuff

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def toggle_fullscreen_desktop():
 
 async def main():
     pygame.init()
-    screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((1920, 1080))
     clock = pygame.time.Clock()
     pygame.display.set_caption("Plant Courier")
 
